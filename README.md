661fbe30348f37ed9f66a8f454869de7f9b9eb63
ganariya <ganariya2525@gmail.com>
2022-07-17 20:42:33 +0900

    :sparkles: init

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# competitive
+
+For competitive repository
+
+# vscode
+
+Code Runner `Ctrl + Option + N`
+
+# environment
+
+- [Mac における GCC の導入](https://qiita.com/DaikiSuyama/items/09f5aa399aad37783146)
+- [デバッガの設定](https://qiita.com/ageprocpp/items/d5111699f9e8a0d88c89)
---
+++ go/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// ------------------------------------------------------------
+// ------------------------------------------------------------
+
+type Io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+func NewIo() *Io {
+	return &Io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+func (io *Io) Flush() {
+	err := io.writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (io *Io) NextLine() string {
+	var buffer []byte
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer = append(buffer, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buffer)
+}
+
+func (io *Io) Next() string {
+	for io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+	r := io.tokens[io.nextToken]
+	io.nextToken++
+	return r
+}
+
+func (io *Io) NextInt() int {
+	i, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) NextFloat() float64 {
+	i, err := strconv.ParseFloat(io.Next(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) PrintLn(a ...interface{}) {
+	fmt.Fprintln(io.writer, a...)
+}
+
+func (io *Io) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format, a...)
+}
+
+func (io *Io) PrintIntLn(a []int) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func (io *Io) PrintStringLn(a []string) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+var mio = NewIo()
+
+// ------------------------------------------------------------
+// ------------------------------------------------------------
+
+const M = 5000
+
+func solve() {
+}
+
+// ------------------------------------------------------------
+// ------------------------------------------------------------
+
+func main() {
+	solve()
+	defer mio.Flush()
+}
